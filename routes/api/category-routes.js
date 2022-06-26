00baadf17c2e0f739c5047f02a3b9d2c52c40ad0
erik escobedo <erikscbd@gmail.com>
2022-06-26 11:13:24 -0500

    updated routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,34 +12,47 @@ router.get('/', async (req, res) => {
   res.json(categories);
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findOne({
+  const category = await Category.findOne({
     where: {
       id: req.params.id,
     },
+    include: [Product],
   });
+  res.json(category);
 });
 
-router.post("/", (req, res) => {
+
+router.post("/", async (req, res) => {
   // create a new category
-  Category.create(req.body)
+  const newCategory = await Category.create(req.body);
+  res.json(newCategory);
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
-  Category.update()
+ const updatedCategory = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  });
+  res.json(updatedCategory);
 });
 
 router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
-  Category.destroy({
-    where: req.params.id,
-  })
+  await Category.destroy({
+    where:{
+      id: req.params.id,
+    },
+  });
+  res.json({ message: 'Deleted'})
 });
 
 module.exports = router;
 
 
 //Mini project Student Project activity 28 for reference 
+
